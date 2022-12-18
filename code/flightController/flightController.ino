18923f7aa95d9f110e86f4b882bae768fa406299
joey101 <jawad.tanana@gmail.com>
2022-12-19 09:41:30 +1100

    Update flightController.ino

--- code/flightController/flightController.ino
+++ code/flightController/flightController.ino
@@ -1,8 +1,18 @@
 #include wire.h
 #include servo.h
 
+// Customize here pulse lengths as needed
+#define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
+#define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("--------------Serial Monitor Begin-------------");
+  Serial.println("--------------Drone Bitch-------------");
+  
+  
+  
+  
   
 }
 
