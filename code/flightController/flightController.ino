1f4d3ce1aeefb76699febc706d64937f7c68f16b
joey101 <jawad.tanana@gmail.com>
2022-12-19 10:13:54 +1100

    Update flightController.ino

--- code/flightController/flightController.ino
+++ code/flightController/flightController.ino
@@ -4,6 +4,9 @@
 #include <Servo.h>
 #include <tuple>
 
+// Initialising object
+Adafruit_MPU6050 mpu;
+
 // Customize here pulse lengths as needed
 #define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
 #define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
@@ -23,6 +26,7 @@ void setup() {
   }
  
   	Serial.println("--------------Serial Monitor Begin-------------");
+   Serial.println("--------------MPU 6050 Begin-------------");
   	Serial.println("--------------Drone Bitch-------------");
    
   
@@ -31,7 +35,26 @@ void setup() {
 
 void loop() { }
 
-std::tuple getLevel() { }
+std::tuple<double, double, double> getLevel() { 
+  sensors_event_t g;
+  bool success = mpu.getGyroSensor()->getEvent(&g);
+  if (!success) {
+    Serial.println("Error getting gyro results");
+    return;
+  }
+  double x = int(g.gyro.x * 180/M_PI) % 360;
+  double y = int(g.gyro.y * 180/M_PI) % 360;
+  double z = int(g.gyro.z * 180/M_PI) % 360;
+  Serial.print("Gyro -> (");
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print(y);
+  Serial.print(",");
+  Serial.print(z);
+  Serial.println(")");
+
+  return std::tuple<double,double,double>{x,y,z};
+}
 
 void goUp() { }
 void goDown() { }
