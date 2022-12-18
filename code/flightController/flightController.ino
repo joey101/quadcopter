44f7c3c912a5183a793a82c04fb44a5e763fcf79
joey101 <jawad.tanana@gmail.com>
2022-12-19 09:48:45 +1100

    Update flightController.ino

--- code/flightController/flightController.ino
+++ code/flightController/flightController.ino
@@ -1,6 +1,9 @@
-#include wire.h
-#include servo.h
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Servo.h>
 
+#include
 // Customize here pulse lengths as needed
 #define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
 #define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
@@ -9,35 +12,18 @@ void setup() {
   Serial.begin(9600);
   Serial.println("--------------Serial Monitor Begin-------------");
   Serial.println("--------------Drone Bitch-------------");
-  
-  
-  
-}
-
-void loop() {
+   
   
   
 }
 
-void goUp() {
-  
-  
-}
+void loop() { }
 
-void goDown() {
-  
-  
-}
 
-void goLeft() {
-  
-  
-}
-
-void goRight() {
-  
-  
-}
+void goUp() { }
+void goDown() { }
+void goLeft() { }
+void goRight() { }
 
 
 
