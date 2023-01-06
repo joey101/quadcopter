8c9eb05605bd3b5b2d6596e0942ba7c16c4ebaaa
joey101 <jawad.tanana@gmail.com>
2023-01-06 16:53:57 +1100

    push

--- code/ESC-calibration-master/ESC-calibration-master/ESC-calibration/ESC-calibration.ino
+++ code/ESC-calibration-master/ESC-calibration-master/ESC-calibration/ESC-calibration.ino
@@ -17,12 +17,15 @@
 #define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
 #define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
 // ---------------------------------------------------------------------------
-Servo motA;//, motB, motC, motD;
+Servo motA, motB, motC, motD;
 char data;
 // ---------------------------------------------------------------------------
 
 void setpower(float f) {
         motA.writeMicroseconds(MIN_PULSE_LENGTH + (f * 1000));
+        motB.writeMicroseconds(MIN_PULSE_LENGTH + (f * 1000));
+        motC.writeMicroseconds(MIN_PULSE_LENGTH + (f * 1000));
+        motD.writeMicroseconds(MIN_PULSE_LENGTH + (f * 1000));
 }
 
 
@@ -33,19 +36,25 @@ void setup() {
     Serial.begin(9600);
     
     motA.attach(4, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    //motB.attach(5, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    //motC.attach(6, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-    //motD.attach(7, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motB.attach(3, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motC.attach(2, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motD.attach(5, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    Serial.println("Before While");
     while (1) {
       float f = Serial.parseFloat(SKIP_ALL, '\n');
+      // Serial.print("IN While: ");
+      // Serial.println(f);
       if (f  == 1.0) {
         break;
       }      
     }
+    Serial.println("Setting Low");
     setpower(0);
     delay(7000);
+    Serial.println("Setting High");
     setpower(1);
     delay(3000);
+    Serial.println("Setting Low");
     setpower(0);
     
 
@@ -59,6 +68,7 @@ void setup() {
 void loop(){
   float f = analogRead(A0);
   float pow = (f / 1023);
+  Serial.print("Reading Power: ");
   Serial.println(pow);
   setpower(pow);
   delay(100);  
@@ -79,17 +89,17 @@ void oldloop() {
             // 0
             case 48 : Serial.println("Sending minimum throttle");
                       motA.writeMicroseconds(MIN_PULSE_LENGTH);
-                      //motB.writeMicroseconds(MIN_PULSE_LENGTH);
-                      //motC.writeMicroseconds(MIN_PULSE_LENGTH);
-                      //motD.writeMicroseconds(MIN_PULSE_LENGTH);
+                      motB.writeMicroseconds(MIN_PULSE_LENGTH);
+                      motC.writeMicroseconds(MIN_PULSE_LENGTH);
+                      motD.writeMicroseconds(MIN_PULSE_LENGTH);
             break;
 
             // 1
             case 49 : Serial.println("Sending maximum throttle");
                       motA.writeMicroseconds(MAX_PULSE_LENGTH);
-                      //motB.writeMicroseconds(MAX_PULSE_LENGTH);
-                      //motC.writeMicroseconds(MAX_PULSE_LENGTH);
-                      //motD.writeMicroseconds(MAX_PULSE_LENGTH);
+                      motB.writeMicroseconds(MAX_PULSE_LENGTH);
+                      motC.writeMicroseconds(MAX_PULSE_LENGTH);
+                      motD.writeMicroseconds(MAX_PULSE_LENGTH);
             break;
 
             // 2
@@ -117,18 +127,18 @@ void test()
         Serial.println(i);
         
         motA.writeMicroseconds(i);
-        //motB.writeMicroseconds(i);
-        //motC.writeMicroseconds(i);
-        //motD.writeMicroseconds(i);
+        motB.writeMicroseconds(i);
+        motC.writeMicroseconds(i);
+        motD.writeMicroseconds(i);
         
         delay(200);
     }
 
     Serial.println("STOP");
     motA.writeMicroseconds(MIN_PULSE_LENGTH);
-    //motB.writeMicroseconds(MIN_PULSE_LENGTH);
-    //motC.writeMicroseconds(MIN_PULSE_LENGTH);
-    //motD.writeMicroseconds(MIN_PULSE_LENGTH);
+    motB.writeMicroseconds(MIN_PULSE_LENGTH);
+    motC.writeMicroseconds(MIN_PULSE_LENGTH);
+    motD.writeMicroseconds(MIN_PULSE_LENGTH);
 }
 
 /**
